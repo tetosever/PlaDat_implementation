427dcd2dee13bd8aabf8a7169f19b08c3500c82e
Mattia Piazzalunga <mattiapiazzalunga@outlook.com>
2023-01-12 01:21:12 +0100

    add filter function

--- ToDo/src/main/resources/static/js/home.js
+++ ToDo/src/main/resources/static/js/home.js
@@ -8,50 +8,48 @@ $(document).ready(
                 whoo = id;
                 switch (whoo) {
                     case "f-t-id": {
-                        $("#"+whoo+" > form > input").val(tId);
+                        $("#" + whoo + " > form > input").val(tId);
                         break;
                     }
                     case "f-t-title": {
-                        $("#"+whoo+" > form > input").val(tTile);
+                        $("#" + whoo + " > form > input").val(tTile);
                         break;
                     }
                     case "f-t-priority": {
-                        $("#"+whoo+" > form > select").val(tPriority);
+                        $("#" + whoo + " > form > select").val(tPriority);
                         break;
                     }
                     case "f-t-name": {
-                        $("#"+whoo+" > form > input").val(tName);
+                        $("#" + whoo + " > form > input").val(tName);
                         break;
                     }
                     case "f-e-id": {
-                        $("#"+whoo+" > form > input").val(eId);
+                        $("#" + whoo + " > form > input").val(eId);
                         break;
                     }
                     case "f-e-title": {
-                        $("#"+whoo+" > form > input").val(eTitle);
+                        $("#" + whoo + " > form > input").val(eTitle);
                         break;
                     }
                     case "f-e-date": {
-                        $("#"+whoo+" > form > input").val(eDate);
+                        $("#" + whoo + " > form > input").val(eDate);
                         break;
                     }
                     case "f-e-user": {
-                        $("#"+whoo+" > form > input").val(eName);
+                        $("#" + whoo + " > form > input").val(eName);
                         break;
                     }
-                };
+                }
+                ;
                 $("#" + whoo + " > .new-filter").css("display", "block");
             }
         });
         $(".new-filter > span").click((e) => {
             let text;
-            if($("#" + whoo + ">form > input").length)
-            {
-                text=$("#" + whoo + ">form>input").val().trim();
-            }
-            else
-            {
-                text=$("#" + whoo + ">form>select").val();
+            if ($("#" + whoo + ">form > input").length) {
+                text = $("#" + whoo + ">form>input").val().trim();
+            } else {
+                text = $("#" + whoo + ">form>select").val();
             }
             if (text != "") {
                 switch (whoo) {
@@ -87,9 +85,9 @@ $(document).ready(
                         eName = text;
                         break;
                     }
-                };
+                }
+                ;
                 $("#" + whoo).addClass("filter-active");
-                sendRequest(whoo);
             } else {
                 switch (whoo) {
                     case "f-t-id": {
@@ -112,7 +110,7 @@ $(document).ready(
                         eId = "";
                         break;
                     }
-                    case "f-f-title": {
+                    case "f-e-title": {
                         eTitle = "";
                         break;
                     }
@@ -125,6 +123,7 @@ $(document).ready(
                         break;
                     }
                 }
+                sendRequest(whoo);
                 $("#" + whoo).removeClass("filter-active");
 
             }
@@ -132,43 +131,6 @@ $(document).ready(
             $("#" + whoo + " >form").find("input").val("");
             $("#" + whoo + " >form").find("select").val("");
         });
-        $(".card-event").click((e) => {
-            let id = e.target.id;
-            let element = events.find(ele => ele.id == id);
-            id = id.toString().replaceAll('-', '');
-            if (element != null) {
-                $('#modalEvent form').attr("method", "post");
-                $('#modalEvent form').attr("action", "/events/update/" + id);
-                $('#modalEvent input[name^="title"]').val(element.title);
-                $('#modalEvent input[name^="title"]').addClass("input-form-focus");
-                $('#modalEvent input[name^="start_date"]').val(element.start_date);
-                if (element.start_date != null && element.start_date != "") {
-                    $('#modalEvent input[name^="start_date"]').addClass("input-form-focus");
-                }
-                $('#modalEvent input[name^="end_date"]').val(element.end_date);
-                if (element.end_date != null && element.end_date != "") {
-                    $('#modalEvent input[name^="end_date"]').addClass("input-form-focus");
-                }
-                $('#modalEvent input[name^="place"]').val(element.place);
-                if (element.place != null && element.place != "") {
-                    $('#modalEvent input[name^="place"]').addClass("input-form-focus");
-                }
-                let tmp = element.usersList.map(a => a.id);
-                $('#modalEvent .ss1').val(tmp);
-                if (tmp != null && tmp != []) {
-                    $('#modalEvent .ss1').addClass("input-form-focus");
-                }
-                $('#modalEvent .ss2').val(element.directory.id);
-                if (element.directory.id != null && element.directory.id != "") {
-                    $('#modalEvent .ss2').addClass("input-form-focus");
-                }
-                $('#modalEvent .modal-footer > a').css("display", "block");
-                $('#modalEvent .modal-footer > a').attr("href", "/events/delete/" + id);
-                $('#modalEvent .modal-subtitles > p').text("Working with the event...")
-                $('#modalEvent').modal('show');
-            }
-        });
-
         $(".createEvent").click(() => {
             $('#modalEvent .modal-subtitles > p').text("Creating an event...")
             $('#modalEvent form').attr("method", "post");
@@ -180,38 +142,6 @@ $(document).ready(
         $('#modalEvent').on('hidden.bs.modal', () => {
             $(".input-form").val("");
         })
-        $(".card-task").click((e) => {
-            let id = e.target.id;
-            let element = tasks.find(ele => ele.id == id);
-            id = id.toString().replaceAll('-', '');
-            if (element != null) {
-                $('#modalTask form').attr("method", "post");
-                $('#modalTask form').attr("action", "/tasks/update/" + id);
-                $('#modalTask input[name^="title"]').val(element.title);
-                $('#modalTask input[name^="title"]').addClass("input-form-focus");
-                $('#modalTask input[name^="description"]').val(element.description);
-                if (element.description != null && element.description != "") {
-                    $('#modalTask input[name^="description"]').addClass("input-form-focus");
-                }
-                $('#modalTask .ss3').val(element.priority);
-                if (element.priority != null && element.priority != "") {
-                    $('#modalTask .ss3').addClass("input-form-focus");
-                }
-                let tmp = element.usersList.map(a => a.id);
-                $('#modalTask .ss1').val(tmp);
-                if (tmp != null && tmp != []) {
-                    $('#modalTask .ss1').addClass("input-form-focus");
-                }
-                $('#modalTask .ss2').val(element.directory.id);
-                if (element.directory.id != null && element.directory.id != "") {
-                    $('#modalTask .ss2').addClass("input-form-focus");
-                }
-                $('#modalTask .modal-footer > a').css("display", "block");
-                $('#modalTask .modal-footer > a').attr("href", "/tasks/delete/" + id);
-                $('#modalTask .modal-subtitles > p').text("Working with the task...")
-                $('#modalTask').modal('show');
-            }
-        });
 
         $(".createTask").click(() => {
             $('#modalTask .modal-subtitles > p').text("Creating a task...")
@@ -225,8 +155,75 @@ $(document).ready(
             $(".input-form").val("");
         })
     }
-)
-
+);
+$(document).on('click', '.card-task', (e) => {
+    let id = e.target.id;
+    let element = tasks.find(ele => ele.id == id);
+    id = id.toString().replaceAll('-', '');
+    if (element != null) {
+        $('#modalTask form').attr("method", "post");
+        $('#modalTask form').attr("action", "/tasks/update/" + id);
+        $('#modalTask input[name^="title"]').val(element.title);
+        $('#modalTask input[name^="title"]').addClass("input-form-focus");
+        $('#modalTask input[name^="description"]').val(element.description);
+        if (element.description != null && element.description != "") {
+            $('#modalTask input[name^="description"]').addClass("input-form-focus");
+        }
+        $('#modalTask .ss3').val(element.priority);
+        if (element.priority != null && element.priority != "") {
+            $('#modalTask .ss3').addClass("input-form-focus");
+        }
+        let tmp = element.usersList.map(a => a.id);
+        $('#modalTask .ss1').val(tmp);
+        if (tmp != null && tmp != []) {
+            $('#modalTask .ss1').addClass("input-form-focus");
+        }
+        $('#modalTask .ss2').val(element.directory.id);
+        if (element.directory.id != null && element.directory.id != "") {
+            $('#modalTask .ss2').addClass("input-form-focus");
+        }
+        $('#modalTask .modal-footer > a').css("display", "block");
+        $('#modalTask .modal-footer > a').attr("href", "/tasks/delete/" + id);
+        $('#modalTask .modal-subtitles > p').text("Working with the task...")
+        $('#modalTask').modal('show');
+    }
+});
+$(document).on('click', '.card-event', (e) => {
+    let id = e.target.id;
+    let element = events.find(ele => ele.id == id);
+    id = id.toString().replaceAll('-', '');
+    if (element != null) {
+        $('#modalEvent form').attr("method", "post");
+        $('#modalEvent form').attr("action", "/events/update/" + id);
+        $('#modalEvent input[name^="title"]').val(element.title);
+        $('#modalEvent input[name^="title"]').addClass("input-form-focus");
+        $('#modalEvent input[name^="start_date"]').val(element.start_date);
+        if (element.start_date != null && element.start_date != "") {
+            $('#modalEvent input[name^="start_date"]').addClass("input-form-focus");
+        }
+        $('#modalEvent input[name^="end_date"]').val(element.end_date);
+        if (element.end_date != null && element.end_date != "") {
+            $('#modalEvent input[name^="end_date"]').addClass("input-form-focus");
+        }
+        $('#modalEvent input[name^="place"]').val(element.place);
+        if (element.place != null && element.place != "") {
+            $('#modalEvent input[name^="place"]').addClass("input-form-focus");
+        }
+        let tmp = element.usersList.map(a => a.id);
+        $('#modalEvent .ss1').val(tmp);
+        if (tmp != null && tmp != []) {
+            $('#modalEvent .ss1').addClass("input-form-focus");
+        }
+        $('#modalEvent .ss2').val(element.directory.id);
+        if (element.directory.id != null && element.directory.id != "") {
+            $('#modalEvent .ss2').addClass("input-form-focus");
+        }
+        $('#modalEvent .modal-footer > a').css("display", "block");
+        $('#modalEvent .modal-footer > a').attr("href", "/events/delete/" + id);
+        $('#modalEvent .modal-subtitles > p').text("Working with the event...")
+        $('#modalEvent').modal('show');
+    }
+});
 $(document).mouseup((e) => {
     let element = $(".new-filter");
     if (!element.is(e.target) && element.has(e.target).length === 0) {
@@ -237,14 +234,29 @@ $(document).mouseup((e) => {
 });
 
 function sendRequest(w) {
-    if ($("#"+w).hasClass("f-events")) {
+    if ($("#" + w).hasClass("f-events")) {
         $.get("/events/read/filter?id=" + eId + "&title=" + eTitle + "&start_date=" + eDate + "&user=" + eName, function (data) {
-            alert("Load was performed.");
+            $(".events-body").html("");
+            if(data!=null)
+            {
+                data.forEach((element) => {
+                    $(".events-body").append(
+                        "<div class='card-event' id='" + element.id + "'><div class='events-body-head'><div><p>" + element.start_date.substring(8,10) + "</p><p>" + element.start_date.substring(5,7) + "</p><p>" + element.title + "</p></div></div></div>"
+                    )
+                });
+            }
         });
     } else {
         $.get("/tasks/read/filter?id=" + tId + "&title=" + tTile + "&priority=" + tPriority + "&user=" + eName, function (data) {
-            alert("Load was performed.");
+            $(".task-body").html("");
+            id(data!=null)
+            {
+                data.forEach((element) => {
+                    $(".task-body").append(
+                        "<div class='card-task' id='"+element.id+"'><div class='task-body-head'><div><svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-list-nested' viewBox='0 0 16 16'><path fill-rule='evenodd'd='M4.5 11.5A.5.5 0 0 1 5 11h10a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm-2-4A.5.5 0 0 1 1 3h10a.5.5 0 0 1 0 1H1a.5.5 0 0 1-.5-.5z'/></svg><p>"+element.title+"</p></div></div></div>"
+                    )
+                });
+            }
         });
     }
-
 }
